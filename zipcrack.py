89f7eb1970e8ea71292fd75c18dfa222bfc77409
JcbbehJdh <74997152+JcbbehJdh@users.noreply.github.com>
2021-01-08 20:18:36 -0500

    Update zipcrack.py

--- zipcrack.py
+++ zipcrack.py
@@ -1,16 +1,14 @@
 import zipfile
 from tqdm import tqdm
 
-# the password list path you want to use, must be available in the current directory
 wordlist = input("Enter Password list: ")
-# the zip file you want to crack its password
+
 zip_file = input("Enter zip file: ")
 
-# initialize the Zip File object
+#
 zip_file = zipfile.ZipFile(zip_file)
-# count the number of words in this wordlist
-n_words = len(list(open(wordlist, "rb")))
-# print the total number of passwords
+
+
 print("Total passwords to test:", n_words)
 
 with open(wordlist, "rb") as wordlist:
